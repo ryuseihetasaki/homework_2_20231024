11d111b448510d1853d7f5a6b82c9bdbe699fb37
ryuseihetasaki <hetasakiryusei73560@gmail.com>
2023-10-30 22:12:30 +0900

    プロジェクト ファイルを追加します。

---
+++ homework_2_20231024/Enemy.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class Enemy {
+public:
+    Enemy(char* pName, int hp);
+    ~Enemy();
+
+    void operator*(const int i);
+
+public:
+    int getDiffence() { return _diffence; }
+    int getHp() { return _hp; }
+    void setHp(int hp) { _hp = hp; }
+    void show();
+
+public:
+    void attack(Hero* hero);
+    void heal();
+
+private:
+    char* _pName;
+    int _nameSize;
+
+private:
+    int _hp = 0;
+    int _attack = 75;
+    int _diffence = 20;
+};
---
+++ homework_2_20231024/Hero.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Hero {
+public:
+    Hero(char* pName, int hp);
+    ~Hero();
+
+public:
+    int getDiffence() { return _diffence; }
+    int getHp() { return _hp; }
+    void setHp(int hp) { _hp = hp; }
+    void show();
+
+public:
+    void attack(Enemy* enemy);
+    void heal();
+
+private:
+    char* _pName;
+    int _nameSize;
+
+private:
+    int _hp = 0;
+    int _attack = 75;
+    int _diffence = 20;
+};
